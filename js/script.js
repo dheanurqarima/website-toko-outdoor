9a3e3eb6b55e6e88d8741f92bd532d888c71b3e2
Dhea <dheanurqarima@gmail.com>
2025-12-15 20:28:57 +0700

    Update tampilan website

--- js/script.js
+++ js/script.js
@@ -1,16 +1,72 @@
-// Toggle class Active
+// Toggle class Active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-//klik diluar navbar untuk menghilangkan hamburger
+//toggle active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//toggle active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+//ketika shopping cart di klik
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+//klik diluar elemen untuk menutup elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-btn");
+const shoppingBtn = document.querySelector("#shopping-cart-btn");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!shoppingBtn.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// modal box toggle active
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailBtns = document.querySelectorAll(".item-detail-btn");
+
+itemDetailBtns.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+itemDetailBtns.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
+
+//klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+//klik diluar container modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
